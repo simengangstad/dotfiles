4463a2b8a82d1f83bc86d8d794093c99f327d689
Simen Gangstad <simen.gangstad@me.com>
2020-01-26 22:23:24 +0100

    Rename setup to bootstrap

---
+++ bootstrap.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+echo "Installing ohmyzsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
--- setup.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-echo "Installing ohmyzsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-

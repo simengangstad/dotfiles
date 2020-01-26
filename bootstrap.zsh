81f7fbe719f79990b81dc78f6c0baa64df678987
Simen Gangstad <simen.gangstad@me.com>
2020-01-26 23:18:48 +0100

    Add alias for git add and commit

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -3,4 +3,7 @@
 echo "Installing ohmyzsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+echo "Setting up aliases for git"
+git config --global alias.lg "log --all --oneline --graph"
+git config --global alias.ac '!git add -A && git commit -m'
 

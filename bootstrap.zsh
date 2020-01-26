28d1027ae7620361d4548d6986555b3c6a9251f5
Simen Gangstad <simen.gangstad@me.com>
2020-01-26 23:09:29 +0100

    Add zsh autocomplete

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -2,4 +2,6 @@
 
 echo "Installing ohmyzsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 

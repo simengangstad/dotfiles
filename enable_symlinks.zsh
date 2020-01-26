0ae997e1a4928b3e0d4385dbd535a01dacf13f0c
Simen Gangstad <simen.gangstad@me.com>
2020-01-26 22:07:17 +0100

    Remove previous dotfiles

--- enable_symlinks.zsh
+++ enable_symlinks.zsh
@@ -4,6 +4,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 for entry in .*; do
 	if [ ! -d $entry ] && [ $entry != '.gitignore' ] && [[ $entry != *.swp ]]; then
+		rm "$HOME/$entry"
 		ln -sv "$SCRIPTPATH/$entry" "$HOME"
 	fi	
 done

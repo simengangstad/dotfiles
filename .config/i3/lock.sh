5e9c152f1809fa6966075d11be2f46d82da9d8ba
Simen Gangstad <simen.gangstad@me.com>
2020-03-26 11:53:42 +0100

    Update colors

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -2,33 +2,31 @@
 
 BG=$HOME/.config/i3/wallpaper.png
 
-BACKGROUND_COLOR='#202020ff'
-BACKGROUND_COLOR_DIMMED='#ffffffff'
+BACKGROUND_COLOR='#4e81edff'
+WHITE='#ffffffff'
 TEXT_COLOR='#ffffffff'
 ERROR_COLOR='#f44336ff'
-INPUT_COLOR='#4e81edff'
 
 $HOME/.config/i3/i3lock-color \
 -i $BG 				 \
 --insidevercolor=$BACKGROUND_COLOR   \
---ringvercolor=$INPUT_COLOR     \
+--ringvercolor=$WHITE \
 \
---insidewrongcolor=$BACKGROUND_COLOR \
+--insidewrongcolor=$ERROR_COLOR \
 --ringwrongcolor=$ERROR_COLOR   \
 \
 --insidecolor=$BACKGROUND_COLOR \
---ringcolor=$BACKGROUND_COLOR_DIMMED        \
---linecolor=$BACKGROUND_COLOR        \
+--ringcolor=$BACKGROUND_COLOR \
+--linecolor='#00000000' \
 --separatorcolor=$BACKGROUND_COLOR   \
 \
 --verifcolor=$TEXT_COLOR        \
 --wrongcolor=$TEXT_COLOR        \
 --timecolor=$TEXT_COLOR        \
 --datecolor=$TEXT_COLOR        \
---greetercolor=#ff00ffff \
 --layoutcolor=$BACKGROUND_COLOR      \
---keyhlcolor=$INPUT_COLOR 	\
---bshlcolor=$INPUT_COLOR 	\
+--keyhlcolor=$WHITE \
+--bshlcolor=$WHITE \
 \
 --screen 1            \
 --clock               \

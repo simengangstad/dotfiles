633a2e1ca6994effb798f124e6d7b0fcfc72cbe6
Simen Gangstad <simen.gangstad@me.com>
2020-04-16 14:34:59 +0200

    Remove darwin related

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,50 +11,44 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Setting up aliases for git"
 git config --global alias.lg "log --all --oneline --graph"
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	echo "Enabling clipboard support"
-	sudo apt-get install xclip
-	echo "Installing vivid for LS_COLORS"
-	wget "https://github.com/sharkdp/vivid/releases/download/v0.5.0/vivid_0.5.0_amd64.deb"
-	sudo apt install ./vivid_*.deb
+echo "Enabling clipboard support"
+sudo apt-get install xclip
+echo "Installing vivid for LS_COLORS"
+wget "https://github.com/sharkdp/vivid/releases/download/v0.5.0/vivid_0.5.0_amd64.deb"
+sudo apt install ./vivid_*.deb
 
-	echo "Installing i3 and dunst"
-	sudo add-apt-repository ppa:kgilmer/speed-ricer
-	sudo apt update
-	mkdir -p $HOME/.config/i3
-	mkdir -p $HOME/.config/dunst
-	mkdir -p $HOME/.icons
-	mkdir -p $HOME/.themes
-	sudo apt install i3-gaps i3blocks fonts-font-awesome rofi xautolock compton dunst
-	
-	echo "Installing playerctl"
-	wget "https://github.com/altdesktop/playerctl/releases/download/v2.1.1/playerctl-2.1.1_amd64.deb"
-	sudo apt install ./playerctl-*.deb
+echo "Installing i3 and dunst"
+sudo add-apt-repository ppa:kgilmer/speed-ricer
+sudo apt update
+mkdir -p $HOME/.config/i3
+mkdir -p $HOME/.config/dunst
+mkdir -p $HOME/.icons
+mkdir -p $HOME/.themes
+sudo apt install i3-gaps i3blocks fonts-font-awesome rofi xautolock compton dunst
 
-	echo "Installing icons, themes and feh"
-	# sudo add-apt-repository ppa:noobslab/icons
-	# sudo apt update && sudo apt install ultra-flat-icons feh 
+echo "Installing playerctl"
+wget "https://github.com/altdesktop/playerctl/releases/download/v2.1.1/playerctl-2.1.1_amd64.deb"
+sudo apt install ./playerctl-*.deb
 
-	echo "Installing i3lock-color"
-	sudo apt install pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev autoconf
-	git clone https://github.com/Raymo111/i3lock-color.git
-	pushd i3lock-color
-		autoreconf -fiv
-	
-		rm -rf build/
-		mkdir -p build 
-		pushd build/
-			../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-			make
-		popd
+echo "Installing i3lock-color"
+sudo apt install pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev autoconf
+git clone https://github.com/Raymo111/i3lock-color.git
+pushd i3lock-color
+	autoreconf -fiv
+
+	rm -rf build/
+	mkdir -p build 
+	pushd build/
+		../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+		make
 	popd
+popd
 
-	echo "Installing powerline fonts"
-	git clone https://github.com/powerline/fonts.git
-	pushd fonts
-		./install.sh
-	popd	
-fi
+echo "Installing powerline fonts"
+git clone https://github.com/powerline/fonts.git
+pushd fonts
+	./install.sh
+popd	
 
 echo "Copying custom zsh theme"
 cp minimized.zsh-theme $HOME/.oh-my-zsh/themes/minimized.zsh-theme
@@ -63,7 +57,4 @@ cp minimized.zsh-theme $HOME/.oh-my-zsh/themes/minimized.zsh-theme
 chsh -s $(which zsh)
 echo "Finished setup, now run PluginInstall in Vim"
 
-if [[ "$OSTYPE" == "darwin" ]]; then
-	echo "Remember to import the minimized theme into Terminal.app"
-fi
 

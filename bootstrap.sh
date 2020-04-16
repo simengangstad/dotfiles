b54ace7f128fb6a027a95d144fecc9f23db65da9
Simen Gangstad <simen.gangstad@me.com>
2020-04-16 14:29:02 +0200

    Use stack for dirs

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,6 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "Setting up aliases for git"
 git config --global alias.lg "log --all --oneline --graph"
-git config --global alias.ac '!git add -A && git commit -m'
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	echo "Enabling clipboard support"
@@ -24,6 +23,8 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	sudo apt update
 	mkdir -p $HOME/.config/i3
 	mkdir -p $HOME/.config/dunst
+	mkdir -p $HOME/.icons
+	mkdir -p $HOME/.themes
 	sudo apt install i3-gaps i3blocks fonts-font-awesome rofi xautolock compton dunst
 	
 	echo "Installing playerctl"
@@ -31,27 +32,28 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	sudo apt install ./playerctl-*.deb
 
 	echo "Installing icons, themes and feh"
-	sudo add-apt-repository ppa:noobslab/icons
-	sudo apt update && sudo apt install ultra-flat-icons gtk2-engines-murrine libglib2.0-dev libxml2-utils feh 
+	# sudo add-apt-repository ppa:noobslab/icons
+	# sudo apt update && sudo apt install ultra-flat-icons feh 
 
 	echo "Installing i3lock-color"
-	git clone https://github.com/Raymo111/i3lock-color.git
-	cd i3lock-color
 	sudo apt install pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev autoconf
-	autoreconf -fiv
+	git clone https://github.com/Raymo111/i3lock-color.git
+	pushd i3lock-color
+		autoreconf -fiv
 	
-	rm -rf build/
-	mkdir -p build && cd build/
-
-	../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-	make
-	cd ../..
+		rm -rf build/
+		mkdir -p build 
+		pushd build/
+			../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+			make
+		popd
+	popd
 
 	echo "Installing powerline fonts"
 	git clone https://github.com/powerline/fonts.git
-	cd fonts
-	./install.sh
-	cd ..
+	pushd fonts
+		./install.sh
+	popd	
 fi
 
 echo "Copying custom zsh theme"

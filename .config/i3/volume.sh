cb73205814afc21a50ee16c43281b0cad3330c1d
Simen Gangstad <simen.gangstad@me.com>
2020-04-17 16:34:17 +0200

    Update blocks

--- .config/i3/volume.sh
+++ .config/i3/volume.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-vol=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master))
-muted=$(awk -F"[][]" '/dB/ { print $6 }' <(amixer sget Master))
-vol=${vol:0:-1}
-
+#vol=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master))
+#muted=$(awk -F"[][]" '/dB/ { print $6 }' <(amixer sget Master))
+vol=($(awk -F"[][]" '{ print $2 }' <(amixer -D pulse sget Master)))
+muted=($(awk -F"[][]" '{ print $4 }' <(amixer -D pulse sget Master)))
+vol=$(echo $vol | sed 's/%//')
 re='^[0-9]+$'
 
 if ! [[ $vol =~ $re ]] ; then
-	echo "" 
+	echo "" 
 elif [ "$muted" = "off" ]; then
-	echo " $vol" 
+	echo " $vol" 
 else
 	if [ $vol -eq 0 ]
 		then 
-			echo " $vol" 
-	elif [ $vol -lt 50 ] 
+			echo "  $vol" 
+	elif [ $vol -lt 35 ] 
 		then
-			echo " $vol" 
-	elif [ $vol -gt 49 ]
+			echo "  $vol" 
+	elif [ $vol -gt 34 ]
 		then 
 			echo " $vol"
 	fi	

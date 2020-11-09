69e0f7d4e6e994f1cdb395c85874d488212c93c8
Simen Gangstad <simen.gangstad@me.com>
2020-11-09 15:40:34 +0100

    Improve changing between laptop and desktop polybars

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+echo $1 
+
 # Launch polybar
-polybar left &
-# polybar right &
+if [ $1 = "desktop" ]; then
+	polybar left &
+	polybar right &
+else
+	polybar laptop &
+fi
+

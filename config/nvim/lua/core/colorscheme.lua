b6b301506ec1266de9a0d8980e56560720e25da2
simengangstad <simen.gangstad@me.com>
2023-07-21 21:25:02 +0200

    Change theme

--- config/nvim/lua/core/colorscheme.lua
+++ config/nvim/lua/core/colorscheme.lua
@@ -1,5 +1,14 @@
+local catppuccin_ok, catppuccin = pcall(require, "catppuccin")
+if not catppuccin_ok then
+	return
+end
+
+catppuccin.setup({
+	flavour = "macchiato",
+})
+
 local status, _ = pcall(vim.cmd, "colorscheme catppuccin")
 if not status then
-    print("Colorscheme not found!")
-    return
+	print("Colorscheme not found!")
+	return
 end

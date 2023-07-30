6115d003a533c18aebbfe618406b5f39591fc824
Simen Gangstad <simen.gangstad@me.com>
2023-07-30 23:27:38 +0200

    Add fetch for latest nvim on linux

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,30 @@
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	sudo apt update && sudo apt upgrade
-	sudo apt install zsh git neovim clang clang-format tmux ccls bat ripgrep black
+	sudo apt install zsh git clang clang-format tmux ccls bat ripgrep black
+
+	# Lazygit
 	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 	tar xf lazygit.tar.gz lazygit
 	sudo install lazygit /usr/local/bin
+
+	# Neovim
+	curl -Lo nvim.tar.gz https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+	tar xf nvim.tar.gz
+	mv nvim-linux64/bin/nvim ~/.local/bin/ 
+	# TODO: move share files
+
+	# Enable zsh
 	chsh -s $(which zsh)
 elif [[ "$OSTYPE" == "darwin20.0" ]]; then
 	# Brew
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	brew install git neovim yabai skhd clang-format alfred tmux ccls bat ripgrep black lazygit
+	brew install git yabai skhd clang-format alfred tmux ccls bat ripgrep black lazygit
 
+	# Neovim
+	curl -Lo nvim.tar.gz https://github.com/neovim/neovim/releases/download/stable/nvim-macos.tar.gz
+	tar xf nvim.tar.gz
 fi
 
 # Packer

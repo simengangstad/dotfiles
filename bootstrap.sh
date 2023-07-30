5ed34a542659d71ec162ed3d197f029bd52e0147
Simen Gangstad <simen.gangstad@me.com>
2023-07-30 13:04:15 +0200

    Add installation of lazygit and packer

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,21 @@
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	sudo apt update && sudo apt upgrade
-	sudo apt install zsh git neovim clang clang-format tmux ccls bat ripgrep black lazygit
+	sudo apt install zsh git neovim clang clang-format tmux ccls bat ripgrep black
+	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+	tar xf lazygit.tar.gz lazygit
+	sudo install lazygit /usr/local/bin
 	chsh -s $(which zsh)
-
 elif [[ "$OSTYPE" == "darwin20.0" ]]; then
 	# Brew
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	brew install git neovim yabai skhd clang-format alfred tmux ccls bat ripgrep black lazygit
+
 fi
+
+# Packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # Oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended

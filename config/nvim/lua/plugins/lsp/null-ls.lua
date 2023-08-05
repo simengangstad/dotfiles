4b708147f77f9fafaa4c3e7f9209cc56c3f66d13
Simen Gangstad <simen.gangstad@me.com>
2023-08-05 22:29:09 +0200

    Update formatting rules for clang-format

--- config/nvim/lua/plugins/lsp/null-ls.lua
+++ config/nvim/lua/plugins/lsp/null-ls.lua
@@ -1,8 +1,7 @@
 local setup, null_ls = pcall(require, "null-ls")
 if not setup then
-    return
+	return
 end
-
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
@@ -11,29 +10,35 @@ local diagnostics = null_ls.builtins.diagnostics
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-    sources = {
-        formatting.stylua,
-        formatting.black,
-        diagnostics.ruff,
-        formatting.clang_format,
-        diagnostics.cpplint.with({
-            args = {
-                "-filter", "-legal/copyright,-build/include_subdir"
-            }
-        }),
-        formatting.cmake_format,
-        diagnostics.cmake_lint,
-    },
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ async = false })
-                end,
-            })
-        end
-    end,
+	sources = {
+		formatting.stylua,
+		formatting.black,
+		diagnostics.ruff,
+		formatting.clang_format.with({
+			args = {
+				"--style",
+				"{ IndentWidth: 4, AllowShortLoopsOnASingleLine: true, AllowShortBlocksOnASingleLine: true, ColumnLimit: 80, BinPackParameters: false, BinPackArguments: false, AllowAllParametersOfDeclarationOnNextLine: false, AlignConsecutiveMacros: true, FixNamespaceComments: false, NamespaceIndentation: All, AlignConsecutiveAssignments: true, AlignEscapedNewlines: true, AlignOperands: Align, AlignTrailingComments: true, AllowAllParametersOfDeclarationOnNextLine: false, AllowAllArgumentsOnNextLine: false, PenaltyBreakAssignment: 50, PointerAlignment: Left, ReferenceAlignment: Left}",
+			},
+		}),
+		diagnostics.cpplint.with({
+			args = {
+				"-filter",
+				"-legal/copyright,-build/include_subdir",
+			},
+		}),
+		formatting.cmake_format,
+		diagnostics.cmake_lint,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
+		end
+	end,
 })

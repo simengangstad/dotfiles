3d738f2d0c4b550ae58044dad43a0f1e33f1c1d4
Simen Gangstad <simen.gangstad@me.com>
2025-02-01 14:19:51 +0100

    Fix <ESC>j/k moving lines

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,50 +2,55 @@
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 
-  sudo apt update && sudo apt upgrade
-  sudo apt install -y zsh git neovim clang-format tmux ccls bat ripgrep make cmake bat fzf python3-venv
+    sudo apt update && sudo apt upgrade
+    sudo apt install -y zsh git neovim clang-format tmux ccls bat ripgrep make cmake bat fzf python3-venv
 
-  # Symlink batcat to bat
-  mkdir -p ~/.local/bin
-  ln -s /usr/bin/batcat ~/.local/bin/bat
+    # Symlink batcat to bat
+    mkdir -p ~/.local/bin
+    ln -s /usr/bin/batcat ~/.local/bin/bat
 
-  # Kitty
-  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-  ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
-  cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-  cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
-  sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-  sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
-  echo 'kitty.desktop' >~/.config/xdg-terminals.list
+    # Kitty
+    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+    ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+    cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+    cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+    sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+    sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+    echo 'kitty.desktop' >~/.config/xdg-terminals.list
 
-  # Lazygit
-  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-  tar xf lazygit.tar.gz lazygit
-  sudo install lazygit /usr/local/bin
-  rm -r lazygit lazygit.tar.gz
+    # Lazygit
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
+    rm -r lazygit lazygit.tar.gz
 
-  # Enable zsh
-  chsh -s $(which zsh)
+    # Enable zsh
+    chsh -s $(which zsh)
 
 elif [[ "$OSTYPE" == "darwin20.0" ]]; then
 
-  # Brew
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  brew install git neovim clang-format tmux ccls bat ripgrep lazygit make cmake bat fzf git-delta duf alfred skhd yabai zoxide
+    # Brew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install git neovim clang-format tmux ccls bat ripgrep lazygit make cmake bat fzf git-delta duf alfred skhd yabai zoxide
 
-  # Kitty
-  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+    # Kitty
+    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
-  # Reduce time for window animations
-  defaults -currentHost write -g NSWindowResizeTime -float 0.065
+    # Reduce time for window animations
+    defaults -currentHost write -g NSWindowResizeTime -float 0.065
 
-  # Disable window opening animations
-  defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+    # Disable window opening animations
+    defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
 fi
 
 # Create config directory
 mkdir ~/.config
+
+# Setup bat theme
+mkdir -p "$(bat --config-dir)/themes"
+wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Macchiato.tmTheme
+bat cache --build
 
 # Rust & cargo
 curl https://sh.rustup.rs -sSf | sh
@@ -64,8 +69,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "Finished setup, next steps:"
 echo "1. Do ./enable_symlinks.zsh"
-echo "2. do bat cache --build"
-echo "3. Install cmake-language-server via pip"
-echo "4. Install Hack Nerd Font Mono"
-echo "5. Remap caps lock to escape (on gnome): gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']""
-echo "6. Log out to refresh the shell"
+echo "2. Install cmake-language-server via pip"
+echo "3. Install Hack Nerd Font Mono"
+echo "4. Remap caps lock to escape (on gnome): gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']""
+echo "5. Log out to refresh the shell"
--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,3 +2,6 @@ vim.opt.relativenumber = false
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.g.root_spec = { "cwd" }
+
+vim.opt.timeoutlen = 1000
+vim.opt.ttimeoutlen = 0
